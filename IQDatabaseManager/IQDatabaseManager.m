a72dda134c923b3bbc2b0e9e0fe039ad8b4c9547
ellinakuznetcova <ellina09.19@gmail.com>
2018-06-13 13:27:12 +0300

    removed database deleting on error

--- IQDatabaseManager/IQDatabaseManager.m
+++ IQDatabaseManager/IQDatabaseManager.m
@@ -140,16 +140,7 @@
             if (![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:optionsDictionary error:&error])
             {
                 NSLog(@"PersistentStore Error: %@, %@", error, [error userInfo]);
-                
-                //Removign file and now trying once again
-                [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil];
-                
-                if (![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error])
-                {
-                    //If issue still not resolved then removing file and aborting.
-                    [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil];
-                    abort();
-                }
+                abort();
             }
         }
     }
